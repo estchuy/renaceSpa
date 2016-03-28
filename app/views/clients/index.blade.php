4bfe02bceea990f9424489a3a08abbf9b22f5e8e
Estuardo Chuy <estchuy@hotmail.com>
2016-03-28 14:37:05 -0600

    index clients

--- app/views/clients/index.blade.php
+++ app/views/clients/index.blade.php
@@ -1,94 +1,92 @@
 @section('content')
-<div class="row mt">
-  <div class="col-md-12">
-      <section class="task-panel tasks-widget">
-    	<div class="panel-heading">
-            <div class="pull-left"><h5><i class="fa fa-tasks"></i> Todo List - Complex Style</h5></div>
-            <br>
-     	</div>
-          <div class="panel-body">
-              <div class="task-content">
-
-                  <ul class="task-list">
-                      <li>
-                          <div class="task-checkbox">
-                              <input type="checkbox" class="list-child" value=""  />
-                          </div>
-                          <div class="task-title">
-                              <span class="task-title-sp">Dashgum - Admin Panel Theme</span>
-                              <span class="badge bg-theme">Done</span>
-                              <div class="pull-right hidden-phone">
-                                  <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
-                                  <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                  <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                              </div>
-                          </div>
-                      </li>
-                      <li>
-                          <div class="task-checkbox">
-                              <input type="checkbox" class="list-child" value=""  />
-                          </div>
-                          <div class="task-title">
-                              <span class="task-title-sp">Extensive collection of plugins</span>
-                              <span class="badge bg-warning">Cool</span>
-                              <div class="pull-right hidden-phone">
-                                  <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
-                                  <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                  <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                              </div>
-                          </div>
-                      </li>
-                      <li>
-                          <div class="task-checkbox">
-                              <input type="checkbox" class="list-child" value=""  />
-                          </div>
-                          <div class="task-title">
-                              <span class="task-title-sp">Free updates always, no extra fees.</span>
-                              <span class="badge bg-success">2 Days</span>
-                              <div class="pull-right hidden-phone">
-                                  <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
-                                  <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                  <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                              </div>
-                          </div>
-                      </li>
-                      <li>
-                          <div class="task-checkbox">
-                              <input type="checkbox" class="list-child" value=""  />
-                          </div>
-                          <div class="task-title">
-                              <span class="task-title-sp">More features coming soon</span>
-                              <span class="badge bg-info">Tomorrow</span>
-                              <div class="pull-right hidden-phone">
-                                  <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
-                                  <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                  <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                              </div>
-                          </div>
-                      </li>
-                      <li>
-                          <div class="task-checkbox">
-                              <input type="checkbox" class="list-child" value=""  />
-                          </div>
-                          <div class="task-title">
-                              <span class="task-title-sp">Hey, seriously, you should buy this Dashboard</span>
-                              <span class="badge bg-important">Now</span>
-                              <div class="pull-right">
-                                  <button class="btn btn-success btn-xs"><i class=" fa fa-check"></i></button>
-                                  <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                  <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                              </div>
-                          </div>
-                      </li>                                      
-                  </ul>
-              </div>
-
-              <div class=" add-task-row">
-                  <a class="btn btn-success btn-sm pull-left" href="todo_list.html#">Add New Tasks</a>
-                  <a class="btn btn-default btn-sm pull-right" href="todo_list.html#">See All Tasks</a>
-              </div>
-          </div>
-      </section>
-  </div><!-- /col-md-12-->
-</div><!-- /row -->
+<section class="wrapper">
+	<div class="row mt">
+	  <div class="col-md-12">
+	      <div class="content-panel">
+	          <table class="table table-striped table-advance table-hover">
+	      	  	  <h4><i class="fa fa-users-right"></i>Clients</h4>
+	      	  	  <hr>
+	              <thead>
+	              <tr>
+	                  <th><i class="fa fa-bullhorn"></i> Company</th>
+	                  <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+	                  <th><i class="fa fa-bookmark"></i> Profit</th>
+	                  <th><i class=" fa fa-edit"></i> Status</th>
+	                  <th></th>
+	              </tr>
+	              </thead>
+	              <tbody>
+	              <tr>
+	                  <td><a href="basic_table.html#">Company Ltd</a></td>
+	                  <td class="hidden-phone">Lorem Ipsum dolor</td>
+	                  <td>12000.00$ </td>
+	                  <td><span class="label label-info label-mini">Due</span></td>
+	                  <td>
+	                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+	                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+	                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+	                  </td>
+	              </tr>
+	              <tr>
+	                  <td>
+	                      <a href="basic_table.html#">
+	                          Dashgum co
+	                      </a>
+	                  </td>
+	                  <td class="hidden-phone">Lorem Ipsum dolor</td>
+	                  <td>17900.00$ </td>
+	                  <td><span class="label label-warning label-mini">Due</span></td>
+	                  <td>
+	                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+	                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+	                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+	                  </td>
+	              </tr>
+	              <tr>
+	                  <td>
+	                      <a href="basic_table.html#">
+	                          Another Co
+	                      </a>
+	                  </td>
+	                  <td class="hidden-phone">Lorem Ipsum dolor</td>
+	                  <td>14400.00$ </td>
+	                  <td><span class="label label-success label-mini">Paid</span></td>
+	                  <td>
+	                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+	                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+	                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+	                  </td>
+	              </tr>
+	              <tr>
+	                  <td>
+	                      <a href="basic_table.html#">
+	                          Dashgum ext
+	                      </a>
+	                  </td>
+	                  <td class="hidden-phone">Lorem Ipsum dolor</td>
+	                  <td>22000.50$ </td>
+	                  <td><span class="label label-success label-mini">Paid</span></td>
+	                  <td>
+	                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+	                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+	                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+	                  </td>
+	              </tr>
+	              <tr>
+	                  <td><a href="basic_table.html#">Total Ltd</a></td>
+	                  <td class="hidden-phone">Lorem Ipsum dolor</td>
+	                  <td>12120.00$ </td>
+	                  <td><span class="label label-warning label-mini">Due</span></td>
+	                  <td>
+	                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+	                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+	                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+	                  </td>
+	              </tr>
+	              </tbody>
+	          </table>
+	      </div><!-- /content-panel -->
+	  </div><!-- /col-md-12 -->
+	</div><!-- /row -->
+</section>
 @stop
