3f979c7828d28dc5c2551f413c9c14a44fc4468d
Estuardo Chuy <estchuy@hotmail.com>
2016-03-30 16:43:38 -0600

    vista para editar clientes

---
+++ app/views/clients/edit.blade.php
@@ -0,0 +1,43 @@
+@section('content')
+<section class="wrapper">
+	<h3>
+		<i class="fa fa-user"></i> Cliente
+	</h3>
+	<div class="row mt">
+	  <div class="col-md-12">
+	      <div class="form-panel">
+	        <div class="form-group">
+              	<label class="col-sm-1 col-sm-2 control-label">Nombre</label>
+               	<div class="col-sm-5">
+                 	<input type="text" id="nombre" name="nombre"  placeholder="Nombre" value="{{$client->name}}" class="form-control round-form">
+               	</div>
+        	</div>
+        	<div class="form-group">
+              	<label class="col-sm-1 col-sm-2 control-label">DPI</label>
+               	<div class="col-sm-5">
+                 	<input type="text" id="dpi" name="dpi"  placeholder="Documento Personal de Identificacion" value="{{$client->personal_id}}" class="form-control round-form">
+               	</div>
+        	</div>
+        	<div class="form-group">
+              	<label class="col-sm-1 col-sm-2 control-label">Direccion</label>
+               	<div class="col-sm-5">
+                 	<input type="text" id="address" name="address"  placeholder="Direccion" value="{{$client->address}}" class="form-control round-form">
+               	</div>
+        	</div>
+        	<div class="form-group">
+              	<label class="col-sm-1 col-sm-2 control-label">Telefono</label>
+               	<div class="col-sm-5">
+                 	<input type="text" id="phone" name="phone"  placeholder="Telefono" value="{{$client->phone}}" class="form-control round-form">
+               	</div>
+        	</div>
+        	<div class="form-group">
+              	<label class="col-sm-1 col-sm-2 control-label">Email</label>
+               	<div class="col-sm-5">
+                 	<input type="text" id="email" name="email"  placeholder="Direccion de Correo Electronico" value="{{$client->email}}" class="form-control round-form">
+               	</div>
+        	</div>
+	      </div><!-- /content-panel -->
+	  </div><!-- /col-md-12 -->
+	</div><!-- /row -->
+</section>
+@stop
