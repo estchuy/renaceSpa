2ca2f211165141e4fa49a9e99a1a3df1336cb803
Estuardo Chuy <estchuy@hotmail.com>
2016-03-31 23:55:39 -0600

    metodo en modelo de prestamos

--- app/models/Loan.php
+++ app/models/Loan.php
@@ -12,4 +12,9 @@ class Loan extends Eloquent {
         'monthly_payment',
         'interest'
     );
+
+    public function getDetailLoans($parentId)
+    {
+        return Loan::where('parent_id', $parent_id)->orderBy('period_id','asc')->get();
+    }
 }
