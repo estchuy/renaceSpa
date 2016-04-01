4446600a8aff29f6ea18fc2d643e327f4d983be9
Estuardo Chuy <estchuy@hotmail.com>
2016-04-01 14:30:51 -0600

    metodos de prestamos

--- app/models/Loan.php
+++ app/models/Loan.php
@@ -17,4 +17,22 @@ class Loan extends Eloquent {
     {
         return Loan::where('parent_id', $parentId)->orderBy('period_id','asc')->get();
     }
+
+    public static function getLoanInfo($client_id)
+    {
+        return Loan::select(DB::raw('count(period_id) as `count_loan`', 'sum(amnt) as `totalLoan`', 'sum(monthly_payment) as `monthly_payment`'))
+        ->where('client_id', $client_id)
+        ->where('period_id', 1)
+        ->groupBy('client_id')
+        ->get();
+    }
+
+    public static function getTotalInteresLoanClient($client_id)
+    {
+        $totalInterest = Loan::select(DB::raw('sum(interest_fee) as totalInterest'))
+        ->where('client_id', $client_id)
+        ->first();
+
+        return $totalInterest->totalInterest;
+    }
 }
