bf3c7cd228f62e7642528d0a4cae2ae9aec4a9e6
Estuardo Chuy <estchuy@hotmail.com>
2016-04-03 12:14:19 -0600

    modification de clientes si no tiene prestamos

--- app/views/clients/edit.blade.php
+++ app/views/clients/edit.blade.php
@@ -56,56 +56,64 @@
           </form>
 	      </div><!-- /content-panel -->
         @if(isset($edit))
-          <!-- Resumen de Pagos -->
-          <div class="form-panel">
-            <div class="form-group">
-              <h4 class="mb"><a onclick="javascript:switchClassi();"><i id="action" class="fa fa-angle-right"></a></i> Historial </h4>
+          @if(count($loans) == 0)
+            <div class="form-panel">
+              <div class="form-group">
+                <button class="btn btn-primary btn-lg" type="button" onclick="javascript:window.location = '/loan/{{$client->id}}/new';">Crear Prestamo</button>
+              </div>
             </div>
-            <div class="form-group" id="loanHistoryDiv" style="display:none;">
-                
-                  <div class="content-panel">
-                    <?php $i = 1;?>
-                    @foreach($loans as $loan)
-                      <table class="table table-striped table-advance table-hover">
-                          <h4><i class="fa fa-users"></i> Prestamo # {{$i}} - <strong>Q{{number_format($loan->amnt, 2, '.', ',')}}</strong></h4>
-                          <hr>
-                          <thead>
-                          <tr>
-                              <th><i class="fa fa-user"></i> Plazo</th>
-                              <th><i class="fa fa-bookmark"></i> Cuotas</th>
-                              <th><i class="fa fa-bookmark"></i> Interes</th>
-                              <th><i class="fa fa-bookmark"></i> Capital</th>
-                              <th><i class="fa fa-bookmark"></i> Saldo</th>
-                              <th><i class=" fa fa-edit"></i> Status</th>
-                          </tr>
-                          </thead>
-                          <tbody>
-                          <?php
-                            $loanDetails = Loan::getDetailLoans($loan->id);
-                          ?>
-                          @foreach($loanDetails as $loanDetail) 
+          @else
+            <!-- Resumen de Pagos -->
+            <div class="form-panel">
+              <div class="form-group">
+                <h4 class="mb"><a onclick="javascript:switchClassi();"><i id="action" class="fa fa-angle-right"></a></i> Historial </h4>
+              </div>
+              <div class="form-group" id="loanHistoryDiv" style="display:none;">
+                  
+                    <div class="content-panel">
+                      <?php $i = 1;?>
+                      @foreach($loans as $loan)
+                        <table class="table table-striped table-advance table-hover">
+                            <h4><i class="fa fa-users"></i> Prestamo # {{$i}} - <strong>Q{{number_format($loan->amnt, 2, '.', ',')}}</strong></h4>
+                            <hr>
+                            <thead>
                             <tr>
-                              <td>{{$loanDetail->period_id}}</td>
-                              <td>Q{{number_format($loanDetail->monthly_payment, 2, '.', ',')}}</td>
-                              <td>Q{{number_format($loanDetail->interest_fee, 2, '.', ',')}}</td>
-                              <td>Q{{number_format($loanDetail->capital, 2, '.', ',')}}</td>
-                              <td>Q{{number_format($loanDetail->balance, 2, '.', ',')}}</td>
-                              <td>
-                                @if($loanDetail->pay == 0)
-                                  <span class="badge bg-important">Pendiente</span>
-                                @else
-                                  <span class="badge bg-success">Pagado</span>
-                                @endif
-                              </td>
+                                <th><i class="fa fa-user"></i> Plazo</th>
+                                <th><i class="fa fa-bookmark"></i> Cuotas</th>
+                                <th><i class="fa fa-bookmark"></i> Interes</th>
+                                <th><i class="fa fa-bookmark"></i> Capital</th>
+                                <th><i class="fa fa-bookmark"></i> Saldo</th>
+                                <th><i class=" fa fa-edit"></i> Status</th>
                             </tr>
-                            @endforeach
-                        </tbody>
-                      </table>
-                      <?php $i++;?>
-                      @endforeach
-                  </div><!-- /content-panel -->
-            </div>
-          </div><!-- End loans history -->
+                            </thead>
+                            <tbody>
+                            <?php
+                              $loanDetails = Loan::getDetailLoans($loan->id);
+                            ?>
+                            @foreach($loanDetails as $loanDetail) 
+                              <tr>
+                                <td>{{$loanDetail->period_id}}</td>
+                                <td>Q{{number_format($loanDetail->monthly_payment, 2, '.', ',')}}</td>
+                                <td>Q{{number_format($loanDetail->interest_fee, 2, '.', ',')}}</td>
+                                <td>Q{{number_format($loanDetail->capital, 2, '.', ',')}}</td>
+                                <td>Q{{number_format($loanDetail->balance, 2, '.', ',')}}</td>
+                                <td>
+                                  @if($loanDetail->pay == 0)
+                                    <span class="badge bg-important">Pendiente</span>
+                                  @else
+                                    <span class="badge bg-success">Pagado</span>
+                                  @endif
+                                </td>
+                              </tr>
+                              @endforeach
+                          </tbody>
+                        </table>
+                        <?php $i++;?>
+                        @endforeach
+                    </div><!-- /content-panel -->
+              </div>
+            </div><!-- End loans history -->
+          @endif
         @endif
 	  </div><!-- /col-md-12 -->
 	</div><!-- /row -->
