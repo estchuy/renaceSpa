6ac66bd3cffaaa1a91a7d579f3df153de868e838
Estuardo Chuy <estchuy@hotmail.com>
2016-04-05 09:32:21 -0600

    reports look and fill

--- app/views/loans/consolidado.blade.php
+++ app/views/loans/consolidado.blade.php
@@ -4,14 +4,13 @@
 	<div class="col-md-4"></div>	
 	  <div class="col-md-4">
 	      <div class="content-panel">
-	      	<div class="form-group">
-		       	<label class="control-label"><h4><i class="fa fa-users"></i> Reporte Consolidado</h4></label>
-	            <div class="col-sm-12">
-	             	<hr>
-	            </div>
-			</div>
-			<div class="col-sm-12">
 	          <table class="table table-striped table-advance table-hover">
+	          	<div class="form-group">
+			       	<label class="control-label"><h4><i class="fa fa-users"></i> Reporte Consolidado</h4></label>
+		            <div class="col-sm-12">
+		             	<hr>
+		            </div>
+				</div>
 	              <thead>
 	              <tr>
 	                  <th><i class="fa fa-sort-numeric-asc"></i> Total Prestamos</th>
@@ -22,11 +21,10 @@
 	              <tbody>
 	              	<tr>
 		                <td style="padding-left:60px">{{$totalLoans}}</td>
-		                <td>Q{{number_format($totalMontly, 2, '.', ',')}}</td>
+		                <td><h3><strong>Q{{number_format($totalMontly, 2, '.', ',')}}</strong></h3></td>
 	              	</tr>
 	           	</tbody>
 	          </table>
-	         </div>
 	      </div><!-- /content-panel -->
 	  </div><!-- /col-md-4 -->
 	  <div class="col-md-4"></div>
--- app/views/loans/detallado.blade.php
+++ app/views/loans/detallado.blade.php
@@ -4,33 +4,41 @@
 		<div class="col-md-3"></div>
 	  <div class="col-md-6">
 	      <div class="content-panel">
-	      	<div class="form-group">
-		       	<label class="col-sm-5 control-label"><h4><i class="fa fa-users"></i> Reporte Detallado</h4></label>
-	            <div class="col-sm-12">
-	             	<hr>
-	            </div>
-			</div>
-			<div class="col-sm-12">
 	          <table class="table table-striped table-advance table-hover">
+	          	<div class="form-group">
+			       	<label class="col-sm-5 control-label"><h4><i class="fa fa-users"></i> Reporte Detallado</h4></label>
+		            <div class="col-sm-12">
+		             	<hr>
+		            </div>
+				</div>
 	              <thead>
 	              <tr>
 	                  <th><i class="fa fa-user"></i> Cliente</th>
 	                  <th><i class="fa fa-sort-numeric-asc"></i> Cuota</th>
-	                  <th><i class="fa fa-th"></i> Monto</th>
+	                  <th><i class="fa fa-money"></i> Monto</th>
 	                  <th></th>
 	              </tr>
 	              </thead>
 	              <tbody>
+	              	<?php $totalPay = 0; ?>
 	              @foreach($loans as $loan)	
 	              	<tr>
 		                <td>{{$loan->name}}</td>
 		                <td>{{$loan->period_id."/".$loan->total_of_periods}}</td>
 		                <td>Q{{number_format($loan->monthly_payment, 2, '.', ',')}}</td>
 	              	</tr>
+	              	<?php
+	              		$totalPay = $totalPay + $loan->monthly_payment;
+	              	?>
 	              	@endforeach
 	           	</tbody>
+	           	<thead>
+	           		<tr>
+	           			<th colspan="2"><strong>Total</strong></th>
+	           			<th><h3><strong>Q{{number_format($totalPay, 2, '.', ',')}}</strong></h3></th>
+	           		</tr>
+	           	</thead>
 	          </table>
-	         </div>
 	      </div><!-- /content-panel -->
 	  </div><!-- /col-md-6 -->
 	  <div class="col-md-3"></div>
