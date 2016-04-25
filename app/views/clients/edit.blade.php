e758a528c142c0efacabfc7e681c5f44574bb150
Estuardo Chuy <estchuy@hotmail.com>
2016-04-25 09:26:00 -0600

    fix form

--- app/views/clients/edit.blade.php
+++ app/views/clients/edit.blade.php
@@ -5,20 +5,20 @@
 <div class="row">
   <div class="col-sm-12">
     <div class="card-box">
-      <form action="#" id="wizard-validation-form" novalidate="novalidate">
+      <form action="#" id="wizard-vertical">
         
           <div>
             <h3>Paso 1</h3>
             <section>
                 <div class="form-group clearfix">
                     <label class="col-lg-2 control-label " for="nombre">Nombre y Apellidos </label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-6">
                         <input class="required form-control" style="color:#fc2b6a;" id="nombre" name="nombre" type="text">
                     </div>
                 </div>
-                <div class="input-group clearfix">
+                <div class="form-group clearfix">
                    <label class="col-lg-2 control-label " for="nombre">Cumpleaños </label>
-                   <div class="col-sm-8">
+                   <div class="col-sm-4">
                       <div class="input-group">
                         <input type="text" style="color:#fc2b6a;" class="form-control" placeholder="mm/dd/yyyy" id="datepicker-autoclose">
                         <span class="input-group-addon bg-custom b-0 text-white"><i class="icon-calender"></i></span>
@@ -27,19 +27,19 @@
                 </div>
                 <div class="form-group clearfix">
                     <label class="col-lg-2 control-label " for="nombre">Edad </label>
-                    <div class="col-sm-4">
+                    <div class="col-sm-2">
                         <input type="text" class="required form-control autonumber" style="color:#fc2b6a;" data-v-min="1" data-v-max="150" placeholder="">
                     </div>
                 </div>
                 <div class="form-group clearfix">
                     <label class="col-lg-2 control-label " for="nombre">Estado Civil </label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-6">
                         <input class="required form-control" style="color:#fc2b6a;" id="nombre" name="nombre" type="text">
                     </div>
                 </div>
                 <div class="form-group clearfix">
                     <label class="col-lg-2 control-label " for="nombre">DPI </label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-6">
                         <input class="required form-control" data-mask="9999 99999 9999" style="color:#fc2b6a;" id="nombre" name="nombre" type="text">
                     </div>
                 </div>
@@ -51,30 +51,23 @@
                 </div>
                 <div class="form-group clearfix">
                     <label class="col-lg-2 control-label " for="nombre">Correo </label>
-                    <div class="col-lg-10">
+                    <div class="col-lg-6">
                         <input class="required form-control" style="color:#fc2b6a;" id="nombre" name="nombre" type="text">
                     </div>
                 </div>
                 
                 <div class="form-group clearfix">
-                    <label class="col-lg-12 control-label ">Referido </label>
-                    <div class="select2-container form-control select2" id="s2id_autogen1">
-                      <a tabindex="-1" class="select2-choice" href="javascript:void(0)">   
-                      <span class="select2-chosen" id="select2-chosen-2">Alaska</span>
-                      <abbr class="select2-search-choice-close"></abbr>   
-                      <span role="presentation" class="select2-arrow"><b role="presentation"></b></span></a>
-                      <label class="select2-offscreen" for="s2id_autogen2"></label>
-                      <input type="text" role="button" aria-haspopup="true" class="select2-focusser select2-offscreen" aria-labelledby="select2-chosen-2" id="s2id_autogen2">
-                    </div>
-                    <select class="form-control select2" tabindex="-1" title="" style="display: none;">
+                    <label class="col-lg-2 control-label ">Referido </label>
+                    <select class="form-control select2">
                       <option>Select</option>
-                      <option value="AK">Alaska</option>
-                      <option value="HI">Hawaii</option>
+                      <option value="AK">Estuardo Chuy</option>
+                      <option value="HI">Francisco Flores</option>
                     </select>
+                      
                 </div>
               </section>
               
-              <h3>Step 2</h3>
+              <h3>Paso  2</h3>
               <section>
                 <div class="form-group clearfix">
                   <div class="col-lg-6 col-md-6">
