0dcdf86ab70638bd4fa070a0bd98f808b2a5dd66
Estuardo Chuy <estchuy@hotmail.com>
2016-04-25 10:56:24 -0600

    database change

--- app/database/migrations/2016_04_06_031456_addTableHistoryOfPayment.php
+++ app/database/migrations/2016_04_06_031456_addTableHistoryOfPayment.php
@@ -20,6 +20,77 @@ class AddTableHistoryOfPayment extends Migration {
 			$table->softDeletes();
 			$table->timestamps();
 		});
+
+		Schema::create('diseases', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('client_id');
+			$table->boolean('diabetes');
+			$table->boolean('hiper_arterial');
+			$table->boolean('pro_tiroides');
+			$table->boolean('des_hormonales');
+			$table->boolean('epilesia');
+			$table->boolean('pro_cardiaco');
+			$table->boolean('cancer');
+			$table->boolean('asma');
+			$table->boolean('queloides');
+			$table->boolean('vih');
+			$table->boolean('renal');
+			$table->boolean('higado_graso');
+			$table->boolean('pro_neurologicos');
+			$table->boolean('depresion');
+			$table->boolean('enfer_neuromusculares');
+			$table->boolean('esclerodermia');
+			$table->boolean('psoriasis');
+			$table->boolean('herpes');
+			$table->boolean('pro_respiratorio');
+			$table->boolean('otros');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+
+		Schema::create('medicines', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('client_id');
+			$table->boolean('vit_e');
+			$table->boolean('gingo');
+			$table->boolean('isotretiroina');
+			$table->boolean('esteroidoes');
+			$table->boolean('apirina');
+			$table->boolean('walfarina');
+			$table->boolean('tranquilizantes');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+
+		Schema::create('skin_treatments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('client_id');
+			$table->boolean('retin_a');
+			$table->boolean('hidroquinona');
+			$table->boolean('esteroidoes');
+			$table->boolean('protector_solar');
+			$table->boolean('creamas_antiedad');
+			$table->boolean('tonico');
+			$table->boolean('jabon');
+			$table->boolean('humectante');
+			$table->boolean('serum');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+
+		Schema::table('clients', function(Blueprint $table)
+        {
+            $table->boolean("pregnant");
+            $table->boolean("medic_treatment");
+            $table->string("medic_treatment_detail");
+            $table->interger('age');
+            $table->string("civil_status");
+            $table->string("cumple");
+      		
+        });
 	}
 
 	/**
@@ -30,6 +101,20 @@ class AddTableHistoryOfPayment extends Migration {
 	public function down()
 	{
 		Schema::dropIfExists("pay_loans");
+		Schema::dropIfExists("diseases");
+		Schema::dropIfExists("medicines");
+		Schema::dropIfExists("skin_treatments");
+
+		Schema::table('clients', function(Blueprint $table)
+        {
+            $table->dropColumn("pregnant");
+            $table->dropColumn("medic_treatment");
+            $table->dropColumn("medic_treatment_detail");
+            $table->dropColumn("age");
+            $table->dropColumn("civil_status");
+            $table->dropColumn("cumple");
+      		
+        });
 	}
 
 }
