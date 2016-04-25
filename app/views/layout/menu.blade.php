a7a45af0f1ebc1f87463a8b605a4b2f599816501
Estuardo Chuy <estchuy@hotmail.com>
2016-04-24 18:53:52 -0600

    Cambios de look

--- app/views/layout/menu.blade.php
+++ app/views/layout/menu.blade.php
@@ -1,34 +1,26 @@
-<aside>
-          <div id="sidebar"  class="nav-collapse ">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
-              
-              	  <p class="centered"><a href="profile.html"><img src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/packages/assets/img/dysCHUY.jpg" class="img-circle" width="60"></a></p>
-              	  <h5 class="centered">DYSCHUY</h5>
-		              <li class="mt">
-                      <a href="/clients">
-                          <i class="fa fa-users"></i>
-                          <span>Clients</span>
-                      </a>
-                  </li>
-                  <li>
-                      <a href="/calculoCuotas">
-                          <i class="fa fa-th-large"></i>
-                          <span>Calculo de Cuotas</span>
-                      </a>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-file-text"></i>
-                          <span>Reportes</span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="/reporte/consolidado">Consolidado</a></li>
-                          <li><a  href="/reporte/detallado">Detallado</a></li>
-                          <li><a  href="/reporte/periodo">Por Periodo</a></li>
-                      </ul>
-                  </li>
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
+        <!-- ========== Left Sidebar Start MENU ========== -->
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+                    <!--- Divider -->
+                    <div id="sidebar-menu">
+                        <ul>
+
+                          <li class="text-muted menu-title">Navigation</li>
+
+                            <li>
+                                <a href="/clients" class="waves-effect waves-light"><i class="md icon-people "></i> <span> Clientes </span> </a>
+                            </li>
+                            <li class="has_sub">
+                                <a href="#" class="waves-effect waves-light"><i class="md icon-notebook"></i> <span> Reportes </span> </a>
+                                <ul class="list-unstyled">
+                                    <li><a href="/reporte/consolidado">Consolidado</a></li>
+                                    <li><a href="/reporte/detallado">Panels</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <!-- Left Sidebar End -->
